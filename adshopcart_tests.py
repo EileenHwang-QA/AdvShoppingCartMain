4d3c6f30983447818a4767afb69a660c33e45b85
Eileen Hwang <eileenhwang630@gmail.com>
2022-03-01 02:46:40 -0800

    update sign up , log in

--- adshopcart_tests.py
+++ adshopcart_tests.py
@@ -7,4 +7,11 @@ class adshopcartPositiveTestCases(unittest.TestCase):
     @staticmethod
     def test_adshopcart():
         methods.setUp()
+        methods.create_new_user()
+        methods.check_new_user_account()
+        methods.login_with_new_credential()
+        methods.delete_user()
+        methods.login_with_deleted_user()
         methods.tearDown()
+
+
