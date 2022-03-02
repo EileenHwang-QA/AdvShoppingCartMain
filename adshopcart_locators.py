5a12d70caa7847793eeebdb7552debbaf1b8cd96
Eileen Hwang <eileenhwang630@gmail.com>
2022-03-01 19:33:43 -0800

    update

--- adshopcart_locators.py
+++ adshopcart_locators.py
@@ -5,13 +5,14 @@ adshopcart_register_url = 'https://advantageonlineshopping.com/#/register'
 my_account_url = 'https://advantageonlineshopping.com/#/myAccount'
 my_order_url = 'https://advantageonlineshopping.com/#/MyOrders'
 new_username = fake.user_name()
+if len(new_username) > 15:
+    new_username = new_username[0:14]
 new_password = fake.password()
 first_name = fake.first_name()
 last_name = fake.last_name()
 email = fake.email()
 city = fake.city()
 phone = fake.phone_number()
-# address = fake.address().length > 50
 address = fake.street_address().replace("\n", "")
 province = fake.province_abbr()
 postal_code = fake.postalcode()
