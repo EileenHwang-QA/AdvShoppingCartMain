6815742751eb8a08b773ed29746b2179066e4881
Eileen Hwang <eileenhwang630@gmail.com>
2022-03-02 15:30:52 -0800

    add text,topmenu,contact us variables

--- adshopcart_locators.py
+++ adshopcart_locators.py
@@ -4,6 +4,11 @@ advantage_shopping_cart_url = 'https://advantageonlineshopping.com/#/'
 adshopcart_register_url = 'https://advantageonlineshopping.com/#/register'
 my_account_url = 'https://advantageonlineshopping.com/#/myAccount'
 my_order_url = 'https://advantageonlineshopping.com/#/MyOrders'
+speaker_url = 'https://advantageonlineshopping.com/#/category/Speakers/4'
+tablet_url = 'https://advantageonlineshopping.com/#/category/Tablets/3'
+laptop_url = 'https://advantageonlineshopping.com/#/category/Laptops/1'
+mice_url = 'https://advantageonlineshopping.com/#/category/Mice/5'
+headphone_url ='https://advantageonlineshopping.com/#/category/Headphones/2'
 new_username = fake.user_name()
 if len(new_username) > 15:
     new_username = new_username[0:14]
@@ -17,5 +22,5 @@ address = fake.street_address().replace("\n", "")
 province = fake.province_abbr()
 postal_code = fake.postalcode()
 full_name = f'{first_name} {last_name}'
-
+subject = fake.sentence(nb_words=100)
 
