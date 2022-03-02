a4bc08e7771be49c4f454ebd3a3fd97a7affceaa
Eileen Hwang <eileenhwang630@gmail.com>
2022-03-02 15:33:35 -0800

    add text,topmenu,contact us funtion

--- adshopcart_tests.py
+++ adshopcart_tests.py
@@ -2,7 +2,7 @@ import unittest
 import adshopcart_methods as methods
 import adshopcart_locators as locators
 
-class adshopcartPositiveTestCases(unittest.TestCase):
+class AdvantageShoppingAppPositiveTestCases(unittest.TestCase):
 
     @staticmethod
     def test_adshopcart():
@@ -10,6 +10,9 @@ class adshopcartPositiveTestCases(unittest.TestCase):
         methods.create_new_user()
         methods.check_new_user_account()
         methods.login_with_new_credential()
+        methods.check_text()
+        methods.check_top_nav_menu()
+        methods.check_contact_us_form()
         methods.delete_user()
         methods.login_with_deleted_user()
         methods.tearDown()
