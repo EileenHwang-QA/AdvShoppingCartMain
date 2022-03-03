c7bad9584fa4588813f63298bf110eb13b955b73
Eileen Hwang <eileenhwang630@gmail.com>
2022-03-03 10:52:53 -0800

    update sleep time

--- adshopcart_methods.py
+++ adshopcart_methods.py
@@ -45,13 +45,13 @@ def tearDown():
 
 def create_new_user():
     driver.find_element(By.ID, 'menuUser').click()
-    sleep(3)
+    sleep(2)
     driver.find_element(By.LINK_TEXT, 'CREATE NEW ACCOUNT').click()
-    sleep(3)
+    sleep(1)
 
     if driver.current_url == locators.adshopcart_register_url:
         driver.find_element(By.XPATH, '//h3[text()="CREATE ACCOUNT"]').is_displayed()
-        sleep(0.5)
+        sleep(1)
         print(f'We are at register page- {locators.adshopcart_register_url}')
 
         driver.find_element(By.XPATH, '//input[@name= "usernameRegisterPage"]').click()
@@ -103,7 +103,7 @@ def create_new_user():
         checkbox_promoption = driver.find_element(By.XPATH, '//input[@name= "allowOffersPromotion"]')
         if checkbox_promoption:
             checkbox_promoption.click()
-            sleep(2)
+            sleep(0.5)
             if checkbox_promoption.is_selected():
                 print('Checked "promotion.." checkbox')
 
@@ -113,7 +113,7 @@ def create_new_user():
         checkbox_iagree = driver.find_element(By.XPATH, '//input[@name= "i_agree"]')
         if checkbox_iagree:
             checkbox_iagree.click()
-            sleep(2)
+            sleep(0.5)
             if checkbox_iagree.is_selected():
                 print('Checked "I_agree.." checkbox')
 
@@ -123,7 +123,7 @@ def create_new_user():
         register_btn = driver.find_element(By.ID, 'register_btnundefined')
         if register_btn.is_enabled():
             register_btn.click()
-            sleep(3)
+            sleep(1)
             print('Register button is enable')
 
         else:
@@ -137,10 +137,10 @@ def check_new_user_account():
     # Click by "My Account" button
     if driver.current_url == locators.advantage_shopping_cart_url:
         driver.find_element(By.ID, 'menuUser').click()
-        sleep(2)
+        sleep(1)
 
         driver.find_element(By.XPATH, '//*[@id="loginMiniTitle"]/label[1]').click()
-        sleep(2)
+        sleep(1)
 
         if driver.current_url == locators.my_account_url:
             assert driver.find_element(By.XPATH, f'//label[contains(.,"{locators.full_name}")]').is_displayed()
@@ -149,20 +149,20 @@ def check_new_user_account():
 
     # Click by "My Order" button
         driver.find_element(By.ID, 'menuUser').click()
-        sleep(2)
+        sleep(1)
 
         driver.find_element(By.XPATH, '//*[@id ="loginMiniTitle"]/label[2]').click()
-        sleep(2)
+        sleep(1)
         if driver.current_url == locators.my_order_url:
             assert driver.find_element(By.XPATH, f'//label[contains(.," - No orders - ")]').is_displayed()
             print(f'Test scenario: Navigate to My order page "{locators.my_order_url}", Check " NO orders "-------is passed')
 
     # Click by "Sign Out" button
         driver.find_element(By.ID, 'menuUser').click()
-        sleep(2)
+        sleep(1)
 
         driver.find_element(By.XPATH, '//*[@id ="loginMiniTitle"]/label[3]').click()
-        sleep(2)
+        sleep(1)
 
         if driver.current_url == locators.advantage_shopping_cart_url:
             print(f'Test scenario: Navigate to Sign out page"{locators.advantage_shopping_cart_url}"\n'
@@ -174,13 +174,13 @@ def login_with_new_credential():
     sleep(2)
 
     driver.find_element(By.NAME, "username").send_keys(locators.new_username)
-    sleep(2)
+    sleep(0.5)
 
     driver.find_element(By.NAME, "password").send_keys(locators.new_password)
-    sleep(2)
+    sleep(0.5)
 
     driver.find_element(By.ID, 'sign_in_btnundefined').click()
-    sleep(2)
+    sleep(0.5)
 
     print(f'Test scenario: login with new credential:\n'
           f' Username- "{locators.new_username}",Password- "{locators.new_password}"-------------------is passed')
@@ -188,24 +188,24 @@ def login_with_new_credential():
 
 def delete_user():
     driver.find_element(By.ID, 'menuUser').click()
-    sleep(2)
+    sleep(1)
 
     driver.find_element(By.XPATH, '//*[ @ id = "loginMiniTitle"]/label[1]').click()
     sleep(2)
 
     driver.execute_script("window.scrollTo(0,600);")
-    sleep(2)
+    sleep(0.5)
 
     driver.find_element(By.CLASS_NAME, 'deleteBtnText').click()
-    sleep(2)
+    sleep(0.5)
 
     if driver.find_element(By.ID, 'deleteAccountPopup').is_displayed():
-        sleep(2)
+        sleep(1)
 
         driver.find_element(By.XPATH, '//*[@class="deletePopupBtn deleteRed"]').click()
         sleep(2)
         print(f'Test scenario: Delete new user account Username - "{locators.new_username} -------is passed')
-        sleep(10)
+        sleep(2)
 
     else:
         print('Fail to delete a new user account. Check your code!')
@@ -218,19 +218,19 @@ def login_with_deleted_user():
         sleep(2)
 
         driver.find_element(By.NAME, "username").send_keys(locators.new_username)
-        sleep(2)
+        sleep(0.5)
 
         driver.find_element(By.NAME, "password").send_keys(locators.new_password)
-        sleep(2)
+        sleep(0.5)
 
         driver.find_element(By.ID, 'sign_in_btnundefined').click()
-        sleep(2)
+        sleep(1)
 
         driver.find_element(By.ID, 'signInResultMessage').is_displayed()
-        sleep(2)
+        sleep(1)
 
         driver.find_element(By.XPATH, '//*[@class="closeBtn loginPopUpCloseBtn"]').click()
-        sleep(2)
+        sleep(1)
 
         print(f'Test scenario: Check login with a deleted user account \n'
               f'Username- "{locators.new_username}", password- "{locators.new_password}"\n'
@@ -246,11 +246,12 @@ def check_text():
         driver.find_element(By.XPATH, "//span[contains(., 'dvantage')]").is_displayed()
         print('Test scenario: Check Home logo -------passed')
 
+
     # check speaker text
     if driver.find_element(By.ID, 'speakersTxt').is_displayed():
-        sleep(5)
+        sleep(1)
         driver.find_element(By.ID, 'speakersTxt').click()
-        sleep(2)
+        sleep(1)
         if driver.current_url == locators.speaker_url:
             assert driver.find_element(By.XPATH, "//h3[contains(., 'SPEAKERS')]").is_displayed()
             print(f'Navigate to speaker page "{locators.speaker_url}"')
@@ -260,15 +261,15 @@ def check_text():
             print('Error found.Check Your code!')
 
         driver.back()
-        sleep(2)
+        sleep(1)
 
     # check tablet text
 
         driver.find_element(By.ID, 'tabletsTxt').is_displayed()
-        sleep(2)
+        sleep(1)
 
         driver.find_element(By.ID, 'tabletsTxt').click()
-        sleep(2)
+        sleep(1)
 
         if driver.current_url == locators.tablet_url:
             assert driver.find_element(By.XPATH, "//h3[contains(., 'TABLETS')]").is_displayed()
@@ -278,13 +279,13 @@ def check_text():
             print('Error found.Check Your code!')
 
         driver.back()
-        sleep(2)
+        sleep(1)
 
     # check laptops text
         if driver.find_element(By.ID, 'laptopsTxt').is_displayed():
-            sleep(2)
+            sleep(1)
         driver.find_element(By.ID, 'laptopsTxt').click()
-        sleep(2)
+        sleep(1)
         if driver.current_url == locators.laptop_url:
             assert driver.find_element(By.XPATH, "//h3[contains(., 'LAPTOPS')]").is_displayed()
             print(f'Navigate to laptop page "{locators.laptop_url}"')
@@ -297,9 +298,9 @@ def check_text():
 
     # check mice text
     if driver.find_element(By.ID, 'miceTxt').is_displayed():
-        sleep(2)
+        sleep(1)
         driver.find_element(By.ID, 'miceTxt').click()
-        sleep(2)
+        sleep(1)
         if driver.current_url == locators.mice_url:
             assert driver.find_element(By.XPATH, "//h3[contains(., 'MICE')]").is_displayed()
             print(f'Navigate to mice page "{locators.mice_url}"')
@@ -308,13 +309,13 @@ def check_text():
             print('Error found.Check Your code!')
 
         driver.back()
-        sleep(2)
+        sleep(1)
 
     # check headphones text
     if driver.find_element(By.ID, 'headphonesTxt').is_displayed():
-        sleep(2)
+        sleep(1)
         driver.find_element(By.ID, 'headphonesTxt').click()
-        sleep(2)
+        sleep(1)
         if driver.current_url == locators.headphone_url:
             assert driver.find_element(By.XPATH, "//h3[contains(., 'HEADPHONES')]").is_displayed()
             print(f'Navigate to headphones page "{locators.headphone_url}"')
@@ -325,22 +326,22 @@ def check_text():
         print('Test scenario: Check that SPEAKERS, TABLETS, HEADPHONES, LAPTOPS, MICE texts are displayed------passed.')
 
         driver.back()
-        sleep(2)
+        sleep(1)
 
 def check_top_nav_menu():
     if driver.current_url == locators.advantage_shopping_cart_url:
         driver.find_element(By.LINK_TEXT, 'OUR PRODUCTS').click()
         print('"OUR PRODUCTS" Key is clickable')
-        sleep(2)
+        sleep(1)
         driver.find_element(By.LINK_TEXT, 'SPECIAL OFFER').click()
         print('"SPECIAL OFFER" Key is clickable')
-        sleep(2)
+        sleep(1)
         driver.find_element(By.LINK_TEXT, 'POPULAR ITEMS').click()
         print('"POPULAR ITEMS Key is clickable')
-        sleep(2)
+        sleep(1)
         driver.find_element(By.LINK_TEXT, 'CONTACT US').click()
         print('"CONTACT US Key is clickable')
-        sleep(2)
+        sleep(1)
 
     else:
         print('top menu is Not clickable. Check your code!')
@@ -370,7 +371,7 @@ def check_contact_us_form():
     send_btn = driver.find_element(By.ID, 'send_btnundefined')
     if send_btn.is_enabled():
         send_btn.click()
-        sleep(1)
+        sleep(0.5)
         print('Send button is enable')
 
     else:
